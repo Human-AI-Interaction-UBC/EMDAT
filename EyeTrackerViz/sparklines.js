cc0eb3446f318484bbb4912faf82e9b870e98232
MattGingerich <gingerich.matthew@gmail.com>
2013-03-20 02:48:40 -0700

    Sparklines: using initial offset instead of compression; generating random test data after manual demo finishes.

--- EyeTrackerViz/sparklines.js
+++ EyeTrackerViz/sparklines.js
@@ -12,15 +12,18 @@ function Sparkline(visID, data, minY, maxY, maxLength){
 	graph = this.vis.append("svg:svg").attr("width", "100%").attr("height", "100%");
 
 	// create a line-drawing function to draw the SVG sparkline
-	this.line = (function (spark) {var scale = spark.getScales();
+	this.line = (function (spark, offset) {var scale = spark.getScales();
 			return d3.svg.line()
-			.x(function(d,i) {return scale[0](i); })
+			.x(function(d,i) {return scale[0](i+offset); })
 			.y(function(d)	 {return scale[1](spark.maxY-d); });
 		});
+		
+	// offset the x-position of the line by the remaining window size
+	var offset = maxLength - data.length;
 
 	// display the line by appending an svg:path element with the data line we created above
 	graph.append("svg:path")
-			.attr("d", this.line(this)(this.data))
+			.attr("d", this.line(this, offset)(data))
 			.attr("style", "fill: none; stroke: darkcyan; stroke-width: 1.5px;");
 	
 	return this;
@@ -28,7 +31,7 @@ function Sparkline(visID, data, minY, maxY, maxLength){
 
 Sparkline.prototype.getScales = function() {
 	// scale from domain of values to pixel ranges
-	var x = d3.scale.linear().domain([0, this.data.length]).range([0, this.width]);
+	var x = d3.scale.linear().domain([0, this.maxLength]).range([0, this.width]);
 	var y = d3.scale.linear().domain([0, this.maxY-this.minY]).range([0, this.height]);
 	return [x, y];
 }
@@ -36,14 +39,16 @@ Sparkline.prototype.getScales = function() {
 Sparkline.prototype.addData = function(newData) {
 	// push the new data to the end of the sparkline data
 	this.data.push.apply(this.data, newData);
-	console.log(this.data);
+	var offset = 0;
 	
 	// forget the beginning of the data array once it's length increases past the window size
 	if (this.data.length > this.maxLength){
 		this.data = this.data.slice(-this.maxLength);
+	} else {
+		offset = this.maxLength - this.data.length;
 	}
 	
 	// re-draw the line
 	this.vis.selectAll("path")
-		.attr("d", this.line(this)(this.data));
+		.attr("d", this.line(this, offset)(this.data));
 }
