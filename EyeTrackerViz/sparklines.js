06d778421d2f3873ccd9d4df626fec3de3cf34a5
MattGingerich <gingerich.matthew@gmail.com>
2013-04-09 12:30:18 -0700

    Added boundary lines and max/min values.

--- EyeTrackerViz/sparklines.js
+++ EyeTrackerViz/sparklines.js
@@ -1,8 +1,10 @@
 function Sparkline(visID, data, minY, maxY, maxLength){
+	this.xOffset = 40;
+	this.yOffset = 6;
 	// get visualization width and height in pixels (remove "px" string and convert to number)
 	this.vis = d3.select("#" + visID);
-	this.width = Number(this.vis.style("width").slice(0,-2));
-	this.height = Number(this.vis.style("height").slice(0,-2));
+	this.width = Number(this.vis.style("width").slice(0,-2))-this.xOffset;
+	this.height = Number(this.vis.style("height").slice(0,-2))-this.yOffset*2;
 	this.maxLength = maxLength;
 	this.data = data;
 	this.minY = minY;
@@ -10,6 +12,26 @@ function Sparkline(visID, data, minY, maxY, maxLength){
 
 	// create an SVG element inside the specified div that fully fills the div
 	graph = this.vis.append("svg:svg").attr("width", "100%").attr("height", "100%");
+	graph.append("svg:line")
+		.attr("class", "boundaryLine")
+		.attr("x1", 0).attr("y1", this.height+this.yOffset)
+		.attr("x2", this.width).attr("y2", this.height+this.yOffset)
+		.style("stroke", "black")
+		.style("stroke-width", 2)
+	graph.append("svg:line")
+		.attr("class", "boundaryLine")
+		.attr("x1", 0).attr("y1", this.yOffset)
+		.attr("x2", this.width).attr("y2", this.yOffset)
+		.style("stroke", "black")
+		.style("stroke-width", 2)
+	graph.append("svg:text")
+		.attr("x", this.width)
+		.attr("y", this.yOffset*2)
+		.text(this.maxY);
+	graph.append("svg:text")
+		.attr("x", this.width)
+		.attr("y", this.height+this.yOffset*2)
+		.text(this.minY);
 
 	// create a line-drawing function to draw the SVG sparkline
 	this.line = (function (spark, offset) {var scale = spark.getScales();
@@ -31,8 +53,8 @@ function Sparkline(visID, data, minY, maxY, maxLength){
 
 Sparkline.prototype.getScales = function() {
 	// scale from domain of values to pixel ranges
-	var x = d3.scale.linear().domain([0, this.maxLength]).range([0, this.width]);
-	var y = d3.scale.linear().domain([0, this.maxY-this.minY]).range([0, this.height]);
+	var x = d3.scale.linear().domain([0, this.maxLength-1]).range([0, this.width]);
+	var y = d3.scale.linear().domain([0, this.maxY-this.minY]).range([this.yOffset, this.height+this.yOffset]);
 	return [x, y];
 }
 
